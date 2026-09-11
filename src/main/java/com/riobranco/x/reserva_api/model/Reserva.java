4df820ba9870b806ba304d9b9d85377290d1db9d
Matheus <matheus.nobregamts@gmail.com>
2026-09-10 21:45:01 -0300

    feat: code-review

--- src/main/java/com/riobranco/x/reserva_api/model/Reserva.java
+++ src/main/java/com/riobranco/x/reserva_api/model/Reserva.java
@@ -1,8 +1,15 @@
 package com.riobranco.x.reserva_api.model;
 
-import jakarta.persistence.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "reservas")
--- src/main/java/com/riobranco/x/reserva_api/repository/ReservaRepository.java
+++ src/main/java/com/riobranco/x/reserva_api/repository/ReservaRepository.java
@@ -1,8 +1,8 @@
 package com.riobranco.x.reserva_api.repository;
 
-import com.riobranco.x.reserva_api.model.Reserva;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.riobranco.x.reserva_api.model.Reserva;
 
 public interface ReservaRepository extends JpaRepository<Reserva, Long> {
 
